1069055968d02f9b8b3dd2558b7b980b9b3e1e5e
SATYAM MISHRA <53936345+SATYAM86400@users.noreply.github.com>
2020-07-30 15:50:59 +0530

    Update map-services.js

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -10,6 +10,6 @@ export const mapServices = [
     name: 'Mapbox',
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    url: `https://api.mapbox.com/styles/v1/colbyfayock/ck8c2foj72lqk1jnug0g2haw0/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`
+    url: `https://api.mapbox.com/styles/v1/satyam86400/ckd85ft6x0elw1ipie93vkhjn/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2F0eWFtODY0MDAiLCJhIjoiY2tkODcyaTZ0MHI5bDJ3c2NiczUzbmdobSJ9.s2SGU0KS5jptPWlTikBB9A`
   }
 ];
